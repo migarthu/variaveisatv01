ad4a8c344e32c1f4c8818bf9bded79e7becce324
unknown <arthurmigul542@gmail.com>
2023-08-17 20:03:13 -0300

    atvvvariavel

---
+++ variaveis.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Cadastro {
+    char nome;
+    int idade;
+    int matricula;
+    char endereco;
+    char curso;
+    char periodo;
+    char disciplinas;
+};
+
+int main() {
+    struct Cadastro aluno;
+
+    printf("Cadastro de Aluno\n");
+
+    printf("Nome: Arthur miguel\n ");
+  
+
+    printf("Idade: 18\n ");
+    
+
+    printf("Matricula: UC23100250\n ");
+   
+
+    printf("Endereco: Brasilia, Riacho Fundo I, Chacara 06, Avenida sucupira, casa 11\n ");
+ 
+
+    printf("Curso: Ciencia da computacao\n ");
+  
+    printf("Periodo: Segundo semestre\n ");
+
+
+    printf("Disciplinas: sei la ksakska");
+ 
+
+   
+    return 0;
+}
